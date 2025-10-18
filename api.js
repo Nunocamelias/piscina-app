ff511f88fdb9fc23cf791e7d56eb56fac068c08a
Nuno Silva <nunosilva.piscinas@gmail.com>
2025-10-18 16:13:24 +0100

    Aumentado limite do body-parser para 20MB (upload de imagens grandes)

--- api.js
+++ api.js
@@ -13,7 +13,8 @@ const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '20mb' }));
+app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
 
 // Rota de teste
 app.get('/', (req, res) => {
