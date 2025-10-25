0c33ff144c9a7feb980e3ddadfbf0bada4a094e9
Nuno Silva <nunosilva.piscinas@gmail.com>
2025-10-25 19:20:42 +0100

    Atualizar dependências e configuração React Native após build estável

--- react-native.config.js
+++ react-native.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-    dependencies: {
-      'react-native-document-picker': {
-        platforms: {
-          android: null, // Desativa o autolinking para Android
-        },
+  dependencies: {
+    'react-native-config': {
+      platforms: {
+        android: null, // 🔹 Ignora o build nativo do react-native-config
       },
     },
-  };
+    'react-native-document-picker': {
+      platforms: {
+        android: null, // 🔹 Ignora também o JNI do document-picker
+      },
+    },
+  },
+};
+
